7c21919ebe924e3ae29b8c6d9df989af54427036
SandeepX <sandeeppant024@gmail.com>
2024-08-06 21:49:00 +0545

    :sparkles: login and register for user done, crud for user done

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role',['super_admin', 'artist_manager', 'artist'])->default('artist');
+            $table->string('phone',20)->nullable();
+            $table->date('dob')->nullable();
+            $table->enum('gender',['m','f','o'])->nullable();
+            $table->string('address')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,20 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+
+<script>
+    feather.replace();
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+<script>
+    $('div.alert.alert-danger').not('.alert-important').delay(10000).slideUp(900);
+    $('div.alert.alert-success').not('.alert-important').delay(5000).slideUp(900);
+</script>
+
+
+<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
